420fa731127afcd1d3ed4894dcc0240f8bd787da
joao2dev <beneficil57@gmail.com>
2026-06-20 18:18:41 -0300

    feat: cria UserRepository, GroupRepository, GroupMemberRepository e FriendshipRepository

---
+++ src/main/java/com/example/chatweb/repositories/GroupMemberRepository.java
@@ -0,0 +1,17 @@
+package com.example.chatweb.repositories;
+
+import com.example.chatweb.entity.Group;
+import com.example.chatweb.entity.GroupMember;
+import com.example.chatweb.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface GroupMemberRepository extends JpaRepository<GroupMember, UUID> {
+    List<GroupMember> findByUser(User user);
+    List<GroupMember> findByGroup(Group group);
+    boolean existsByUserAndGroup(User user, Group group);
+
+}
