d5024d15d0a60aa0b8fcc22256a89417ed1abe0d
joao2dev <beneficil57@gmail.com>
2026-07-01 17:15:11 -0300

    feat: implementa sendFriendRequest no FriendshipService com TDD

---
+++ src/main/java/com/example/chatweb/Service/FriendshipService.java
@@ -0,0 +1,47 @@
+package com.example.chatweb.Service;
+
+import com.example.chatweb.entity.Friendship;
+import com.example.chatweb.entity.User;
+import com.example.chatweb.repositories.FriendshipRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FriendshipService {
+    private FriendshipRepository repository;
+
+    Friendship sendFriendRequest(User requester, User addressee){
+
+        if (requester.getId().equals(addressee.getId())){
+            throw new RuntimeException("Voce não pode enivar um convite a si mesmo");
+        }
+
+
+        Optional<Friendship> existente = repository.findByRequesterAndAddressee(requester,addressee);
+        Optional<Friendship> existenteInverso = repository.findByRequesterAndAddressee(addressee, requester);
+
+        if (existente.isPresent() || existenteInverso.isPresent()) {
+            throw new RuntimeException("usuario ja adicionado");
+        }
+
+        Friendship novaAmizade = new Friendship(UUID.randomUUID(),requester,addressee, Friendship.FriendshipStatus.PENDING, LocalDateTime.now());
+        return repository.save(novaAmizade);
+    }
+   /* Friendship acceptFriendRequest(UUID friendshipId){}              // aceitar
+    Friendship declineFriendRequest(UUID friendshipId)  {}           // recusar
+    void deleteFriendship(UUID friendshipId)  {}                     // deletar
+    List<Friendship> getFriends(User user){}                        // listar amigos aceitos
+    List<Friendship> getPendingRequests(User user)   {}   */          // listar pedidos pendentes recebidos
+}
---
+++ src/test/java/com/example/chatweb/Service/FriendshipServiceTest.java
@@ -0,0 +1,82 @@
+package com.example.chatweb.Service;
+
+import com.example.chatweb.entity.Friendship;
+import com.example.chatweb.entity.User;
+import com.example.chatweb.repositories.FriendshipRepository;
+import com.example.chatweb.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class FriendshipServiceTest {
+
+    private User joao;
+    private User maria;
+
+    @BeforeEach
+    public void setup(){
+        this.joao = new User(UUID.randomUUID(), "joao2dev", "joao@gmail.com", "12345");
+        this.maria = new User(UUID.randomUUID(), "maria", "maria@gmail.com", "12345");
+    }
+
+    @Mock
+    private FriendshipRepository friendshipRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private FriendshipService friendshipService;
+
+    @Test
+    void deveLancarExcecaoQuandoUsuarioTentaAdicionarASiMesmo(){
+        RuntimeException thrown = assertThrows(
+                RuntimeException.class,
+                () -> {
+                    friendshipService.sendFriendRequest(joao,joao);
+                }
+
+        );
+        assertEquals("Voce não pode enivar um convite a si mesmo",thrown.getMessage());
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoJaExisteFriendship(){
+        when(friendshipRepository.findByRequesterAndAddressee(joao,maria)).thenReturn(Optional.of(new Friendship(UUID.randomUUID(),joao,maria, Friendship.FriendshipStatus.PENDING, LocalDateTime.now())));
+        RuntimeException thrown = assertThrows(
+                RuntimeException.class,
+                () -> {friendshipService.sendFriendRequest(joao,maria);
+                }
+        );
+        assertEquals("usuario ja adicionado",thrown.getMessage());
+    }
+
+    @Test
+    void deveCriarFriendshipComSucesso(){
+        Friendship friendshipEsperada = new Friendship(
+                UUID.randomUUID(), joao, maria,
+                Friendship.FriendshipStatus.PENDING,
+                LocalDateTime.now()
+        );
+        when(friendshipRepository.findByRequesterAndAddressee(joao,maria)).thenReturn(Optional.empty());
+        when(friendshipRepository.save(any())).thenReturn(friendshipEsperada);
+        Friendship amizade = friendshipService.sendFriendRequest(joao,maria);
+
+        assertEquals(joao, amizade.getRequester());
+        assertEquals(maria, amizade.getAddressee());
+        assertEquals(Friendship.FriendshipStatus.PENDING, amizade.getStatus());
+    }
+}
